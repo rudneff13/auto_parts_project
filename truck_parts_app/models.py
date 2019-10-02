5ca6fd02fece410695912bf6cecd47e83a6d22a0
Dmytro Rudniev <rudneff13@gmail.com>
2019-10-02 15:30:34 +0300

    fixed admin module. finished 3x template-pages. fixed ProductDetailView
    also fixed code by note of Dmitry Shevelev

--- truck_parts_app/models.py
+++ truck_parts_app/models.py
@@ -2,36 +2,36 @@ from django.db import models
 
 
 class Truck(models.Model):
-    name = models.CharField(max_length=50, unique=True, verbose_name='Автомобиль')
-
-    def get_products(self):
-        return '; '.join([p.name for p in self.products.all()])
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(max_length=50, unique=True, verbose_name='Truck')
 
     class Meta:
         verbose_name = 'Truck'
         verbose_name_plural = 'Trucks'
         ordering = ['name']
 
-
-class Product(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Изделие')
-    description = models.CharField(max_length=250, null=True, blank=True, verbose_name='Описание')
-    trucks = models.ManyToManyField(Truck, through='ProductTruck', through_fields=('product', 'truck'),
-                                    symmetrical=True, related_name='products')
-
     def __str__(self):
         return self.name
 
-    def get_trucks(self):
-        return ', '.join([truck.name for truck in self.trucks.all()])
+    def get_products(self):
+        return '; '.join([p.name for p in self.products.all()][:50])
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Product')
+    description = models.CharField(max_length=250, blank=True, verbose_name='Description')
+    trucks = models.ManyToManyField(Truck, through='ProductTruck', through_fields=('product', 'truck'),
+                                    symmetrical=True, related_name='products')
 
     class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
         ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def get_trucks(self):
+        return ', '.join([truck.name for truck in self.trucks.all()][:50])
 
 
 class ProductTruck(models.Model):
